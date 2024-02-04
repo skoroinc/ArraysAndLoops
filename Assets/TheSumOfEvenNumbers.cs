cee8697981d212a250bf56b4e9c22b12dfb45525
Rusichbtw <skoroxod_tt@mail.ru>
2024-02-04 15:00:13 +0300

    Исправил код

--- Assets/TheSumOfEvenNumbers.cs
+++ Assets/TheSumOfEvenNumbers.cs
@@ -1,28 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-
 public class TheSumOfEvenNumbers : MonoBehaviour
 {
     [SerializeField] TMP_Text _textResult;
     private int min = 7;
     private int max = 21;
-    
-
     void Start()
     {
         _textResult.text = "";
     }
-
     public void OnClickTheSumOfEvenNumbers()
     {
-        SumEvenNumbers(min, max);
+        int result = SumEvenNumbers(min, max);
+        _textResult.text = $"Сумма четных чисел в заданном диапозоне, от 7 до 21, равна {result}";
     }
     private int SumEvenNumbers(int min, int max)
     {
-
         int sum = 0;
         for (int i = min; i <= max; i++)
         {
@@ -31,10 +24,6 @@ public class TheSumOfEvenNumbers : MonoBehaviour
                 sum += i;
             }
         }
-        _textResult.text = $"Сумма четных чисел в заданном диапозоне, от 7 до 21, равна {sum}";
         return sum;
     }
-
-
 }
-
--- Assets/TheSumOfEvenNumbersInArray.cs
+++ Assets/TheSumOfEvenNumbersInArray.cs
@@ -1,30 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
 public class TheSumOfEvenNumbersInArray : MonoBehaviour
 {
-
     [SerializeField] TMP_Text _textResult;
     private int[] array = { 81, 22, 13, 54, 10, 34, 15, 26, 71, 68 };
-    
-
-
     private void Start()
     {
         _textResult.text = "";
     }
-
     public void OnClickTheSumOfEvenNumbersInArray()
     {
-        SumEvenNumbersArray();
+        int sum;
+        SumEvenNumbersArray(out sum);
+        _textResult.text = $"Сумма четных чисел в заданном массиве, равна {sum}";
     }
-
-
-    private int SumEvenNumbersArray()
+    private void SumEvenNumbersArray(out int sum)
     {
-        int sum = 0;
+        sum = 0;
         for (int i = 0; i < array.Length; i++)
         {
             if (array[i] % 2 == 0)
@@ -32,7 +24,5 @@ public class TheSumOfEvenNumbersInArray : MonoBehaviour
                 sum += array[i];
             }
         }
-        _textResult.text = $"Сумма четных чисел в заданном массиве, равна {sum}";
-        return sum;
     }
 }
